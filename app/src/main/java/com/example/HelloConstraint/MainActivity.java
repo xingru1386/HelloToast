ea3c2ab1e380cbae3cee2c0e1f384eab00bb0870
kerta_123456 <kuanwy-wm19@student.tarc.edu.my>
2020-07-02 12:40:11 +0800

    commit by Kuan

--- app/src/main/java/com/example/HelloConstraint/MainActivity.java
+++ app/src/main/java/com/example/HelloConstraint/MainActivity.java
@@ -1,7 +1,8 @@
-package com.example.hellotoast;
+package com.example.HelloConstraint;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
@@ -26,7 +27,20 @@ public class MainActivity extends AppCompatActivity {
 
     public void countUp(View view) {
         mCount++;
-        if (mShowCount != null)
+        if (mShowCount != null) {
+            findViewById(R.id.button_zero).setBackgroundColor(Color.MAGENTA);
             mShowCount.setText(Integer.toString(mCount));
+        }
+        if(mCount%2==1)
+            view.setBackgroundColor(Color.GREEN);
+        else
+            view.setBackgroundColor(Color.RED);
+    }
+
+    public void countZero(View view) {
+        mCount=0;
+        mShowCount.setText(Integer.toString(mCount));
+        findViewById(R.id.button_zero).setBackgroundColor(Color.GRAY);
+        findViewById(R.id.button_count).setBackgroundColor(Color.BLUE);
     }
 }
--- app/src/test/java/com/example/HelloConstraint/ExampleUnitTest.java
+++ app/src/test/java/com/example/HelloConstraint/ExampleUnitTest.java
@@ -1,4 +1,4 @@
-package com.example.hellotoast;
+package com.example.HelloConstraint;
 
 import org.junit.Test;
 
